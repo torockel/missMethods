9c3b6292e260306cc2d368e04b3304b856b23f0c
torockel <rockel.to@gmail.com>
2020-07-15 17:02:15 +0200

    Add docu for find_md_patterns()

--- R/utils-md-pattern.R
+++ R/utils-md-pattern.R
@@ -1,4 +1,15 @@
 # helpers for MD patterns ---------------------------------
+
+#' Find missing data patterns
+#'
+#' @param M missing data indicator matrix (normally via: `is.na(ds)`)
+#'
+#' @return A list with two components:
+#' * `pattern_matrix` A matrix of all MD-patterns of `M` (every pattern is only included once)
+#' * `pattern_obj` A numbered list. The entry number `i` contains all objects
+#'   that have MD-pattern in row `i` of `pattern_matrix`
+#'
+#' @noRd
 find_md_patterns <- function(M) {
   patterns <- list()
   pattern_obj <- list()
