aa3076fbbe389123e6d5e1bcb797331493388475
torockel <rockel.to@gmail.com>
2020-07-20 15:58:46 +0200

    Use styler and improve comments

--- R/impute_LS_gene.R
+++ R/impute_LS_gene.R
@@ -39,94 +39,103 @@
 impute_LS_gene <- function(ds, k = 10, eps = 1e-6, min_common_obs = 5,
                            return_r_max = FALSE) {
 
+  ## Check some arguments -----------------------------------------------------
   if (k >= nrow(ds)) {
     stop("k must be smaller as nrow(ds)")
   }
-
   if (min_common_obs < 3) {
-    stop("min_common_obs should be bigger as 2 to allow calculations for correlations and regression models")
+    stop(
+      "min_common_obs should be bigger as 2 to allow calculations for ",
+      "correlations and regression models"
+    )
   }
 
-  if(return_r_max) {
+  ## Define some variables for later ------------------------------------------
+  if (return_r_max) {
     r_max_matrix <- matrix(NA, nrow = nrow(ds), ncol = ncol(ds))
   }
-
   ds_imp <- ds
   M <- is.na(ds)
 
-  for(i in 1:nrow(ds)) {
+  ## Impute row by row --------------------------------------------------------
+  for (i in 1:nrow(ds)) {
     M_i <- M[i, ]
-    if(any(M_i)) {  # only impute, if any missing value in row i
+    if (any(M_i)) { # only impute, if any missing value in row i
 
-      ## check some special cases ---------------------------------------------
-
-      if(all(M_i)) { # all values in row i are missing
-        ## Bo et al. do not impute in this case, they return ds with NA values!
-        warning("No observed value in row " , i, ". This row is imputed with column means.")
+      ## Check for enough observations in row i -------------------------------
+      if (all(M_i)) { # all values in row i are missing
+        # Bo et al. do not impute in this case, they return ds with NA values!
+        warning("No observed value in row ", i, ". This row is imputed with column means.")
         ds_imp[i, ] <- colMeans(ds, na.rm = TRUE)
-
       } else if (sum(!M_i) < min_common_obs) { # less than min_common_obs observed values in row i
-        ## Bo et al. impute all values in these rows with the mean of the observed row values
-        ## (source: try and error with original jar-file, see test-file)
+        # Bo et al. impute all values in these rows with the mean of the observed row values
+        # (source: try and error with original jar-file, see test-file)
         ds_imp[i, M_i] <- mean(ds[i, !M_i])
+      } else {
+        ## Enough observations in row i ------------------------------------
+        # At least min_common_obs (>= 3) observed values in row i
+        # ->  proceed with "normal" LSimpute_gene
 
-      } else { # at least min_common_obs (>= 3) observed values in row i ->  proceed with "normal" LSimpute_gene
-
-        ## LSimpute_gene for one row ------------------------------------------
-
-        ## possible candidate rows (common obs!) for imputing
-
+        # Possible candidate rows (common obs!) for imputing and their correlation
         rows_candidates <- find_rows_candidates(ds, i, M, M_i, min_common_obs)
-
+        # Check if at least one row candidate is found
         if (nrow(rows_candidates) == 0) { # no rows_candidates found -> impute mean
-          ## this condition may crashes the jar-file from Bo et al. (2004)
-          warning("No suitable row for the imputation of row ", i,
-                  " found! This row is imputed with observed row mean.")
+          # this condition may crashes the jar-file from Bo et al. (2004)
+          warning(
+            "No suitable row for the imputation of row ", i,
+            " found! This row is imputed with observed row mean."
+          )
           ds_imp[i, M_i] <- mean(ds[i, ], na.rm = TRUE)
-        } else {# at least one candidate row -> proceed with "normal" LSimpute_gene
+        } else { # at least one candidate row -> proceed with "normal" LSimpute_gene
 
+          # Save calculated regression coefficients (later)
           betas <- matrix(NA_real_, nrow = nrow(rows_candidates), ncol = 2)
-          # idea for betas: colnames(betas) <- c("beta_0", "beta_1")
+          # Idea for betas: colnames(betas) <- c("beta_0", "beta_1")
 
-          ## for every missing value j_ind in row i calculate the imputation value separately
-          for(j_ind in which(M_i)) {
-            ## find the k suitable rows for imputation and save their index from rows_candidates
+          ## Impute value by value in row i -----------------------------------
+          for (j_ind in which(M_i)) {
+            # find the k suitable rows for imputation and save their index from rows_candidates
             suitable_logic <- !M[rows_candidates$row_index, j_ind] # No NA in j_ind -> suitable
             suitable_index <- which(suitable_logic)
             suitable_index <- suitable_index[seq_len(min(k, length(suitable_index)))]
 
             if (length(suitable_index) == 0) { # no suitable row found -> impute mean
-              ## this condition may crashes the jar-file from Bo et al. (2004)
-              warning("No suitable row for the imputation of row ", i,
-                      " and column ", j_ind, " found! Value is imputed with observed row mean.")
+              # This condition may crashes the jar-file from Bo et al. (2004)
+              warning(
+                "No suitable row for the imputation of row ", i,
+                " and column ", j_ind, " found! Value is imputed with observed row mean."
+              )
               ds_imp[i, M_i] <- mean(ds[i, ], na.rm = TRUE)
-
             } else { # everything fine -> proceed with "normal" LSimpute_gene
 
-              if(return_r_max) { # save highest correlation
+              if (return_r_max) { # save highest correlation
                 r_max_matrix[i, j_ind] <- rows_candidates$similarity[suitable_index[1]]
               }
 
-              ## calculate imputation values (with regression) and save them in y
+              ## Calculate imputation values for all k neighbors --------------
+              # Prior to that: check, if any regression coefficient is unknown
               row_indices <- rows_candidates$row_index[suitable_index]
-              for(j in seq_along(row_indices)) { # check for unknown regression parameters
+              for (j in seq_along(row_indices)) { # check for unknown regression parameters
                 if (is.na(betas[suitable_index[j], 1])) { # calculate regression parameters, if not already known
-                  common_observed <- !(M_i | M[row_indices[j], ])  # this will be at least min_common_obs (>= 3) TRUEs, (requirement for suitable) -> regression possible
-                  betas[suitable_index[j], ] <- calc_lm_coefs_simple_reg(ds[i, common_observed],
-                                                                         ds[row_indices[j], common_observed])
+                  common_observed <- !(M_i | M[row_indices[j], ]) # this will be at least min_common_obs (>= 3) TRUEs, (requirement for suitable) -> regression possible
+                  betas[suitable_index[j], ] <- calc_lm_coefs_simple_reg(
+                    ds[i, common_observed],
+                    ds[row_indices[j], common_observed]
+                  )
                 }
               }
+              # Calculate the imputation values
               y <- betas[suitable_index, 1] + betas[suitable_index, 2] * ds[row_indices, j_ind]
 
 
-              ## calculate weights  ---------------------------------------------
+              ## Calculate weights  ---------------------------------------------
               similarities_sq <- rows_candidates$similarity[suitable_index]^2
               w <- (similarities_sq /
-                      ( 1- similarities_sq + eps))^2
+                (1 - similarities_sq + eps))^2
               w <- w / sum(w)
 
 
-              ## calculate final imputation value -------------------------------
+              ## Calculate final imputation value -------------------------------
               ds_imp[i, j_ind] <- sum(w * y)
             }
           }
@@ -135,15 +144,15 @@ impute_LS_gene <- function(ds, k = 10, eps = 1e-6, min_common_obs = 5,
     }
   }
 
-  ## return value -------------------------------------------------------------
-  if(return_r_max) {
+  ## Return value -------------------------------------------------------------
+  if (return_r_max) {
     return(list(imp = ds_imp, r_max = r_max_matrix))
   } else {
     return(ds_imp)
   }
 }
 
-## helpers for LSimpute_gene --------------------------------------------------
+## Helpers for LSimpute_gene --------------------------------------------------
 
 calc_common_obs <- function(M, M_i) {
   nr_vars <- ncol(M)
@@ -161,10 +170,10 @@ calc_common_obs <- function(M, M_i) {
 calc_lm_coefs_simple_reg <- function(y, x) {
   sum_x <- sum(x)
   sum_y <- sum(y)
-  sum_xy <- sum(x*y)
-  sum_x_sq <- sum(x ^2)
+  sum_xy <- sum(x * y)
+  sum_x_sq <- sum(x^2)
   n <- length(x)
-  beta_1 <- (sum_xy - sum_x * sum_y / n) / (sum_x_sq - sum_x ^2 / n)
+  beta_1 <- (sum_xy - sum_x * sum_y / n) / (sum_x_sq - sum_x^2 / n)
   beta_0 <- sum_y / n - beta_1 * sum_x / n
   c(beta_0, beta_1)
 }
@@ -184,7 +193,7 @@ calc_similarity <- function(ds, y) {
 #' @param M_i missing data indicator vector of row i
 #' @param min_common_obs documented in impute_LS_gene()
 #'
-  #' @return a data frame with two columns. First column: indices of candidates;
+#' @return a data frame with two columns. First column: indices of candidates;
 #'  second column: similarity (= abs(correlation)) between row i and candidate rows
 #'  data frame is ordered (decreasing) by similarity
 #' @noRd
@@ -195,11 +204,12 @@ find_rows_candidates <- function(ds, i, M = is.na(ds), M_i = M[i, ], min_common_
     return(data.frame(row_index = integer(0)))
   }
 
-  ## calculate correlation
+  # Calculate correlation
   similarity_i <- calc_similarity(ds[rows_candidates_index, ], ds[i, ])
-  # order rows by similarity
+  # Order rows by similarity
   orderd_index <- order(similarity_i, decreasing = TRUE)
-  data.frame(row_index = rows_candidates_index[orderd_index],
-             similarity = similarity_i[orderd_index])
-
+  data.frame(
+    row_index = rows_candidates_index[orderd_index],
+    similarity = similarity_i[orderd_index]
+  )
 }
