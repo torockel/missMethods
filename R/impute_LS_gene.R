a7b9988cdeb79d90348bf648cea987be6e59d23d
torockel <rockel.to@gmail.com>
2020-07-20 10:05:29 +0200

    Use data.frame to save similarities and row_index together
    - rearrange impute_LS_gene() for new data structure
    - remove find_suitable_rows()

--- R/impute_LS_gene.R
+++ R/impute_LS_gene.R
@@ -70,31 +70,29 @@ impute_LS_gene <- function(ds, k = 10, eps = 1e-6, min_common_obs = 5,
         ## (source: try and error with original jar-file, see test-file)
         ds_imp[i, M_i] <- mean(ds[i, !M_i])
 
-      } else { # at least min_common_obs (>= 3) observed values in row i -> "normal" LSimpute_gene possible
+      } else { # at least min_common_obs (>= 3) observed values in row i ->  proceed with "normal" LSimpute_gene
 
         ## LSimpute_gene for one row ------------------------------------------
 
         ## possible candidate rows (common obs!) for imputing
-        rows_candidates <- which(calc_common_obs(M, M_i) >= min_common_obs & seq_len(nrow(ds)) != i)
 
-        if (length(rows_candidates) == 0) { # no rows_candidates found -> impute mean
+        rows_candidates <- find_rows_candidates(ds, i, M, M_i, min_common_obs)
+
+        if (nrow(rows_candidates) == 0) { # no rows_candidates found -> impute mean
           ## this condition may crashes the jar-file from Bo et al. (2004)
           warning("No suitable row for the imputation of row ", i,
                   " found! This row is imputed with observed row mean.")
           ds_imp[i, M_i] <- mean(ds[i, ], na.rm = TRUE)
-        } else {
-          ## calculate correlation and order rows by correlation
-          similarity_i <- calc_similarity(ds[rows_candidates, ], ds[i, ])
-          names(similarity_i) <- rows_candidates # to save the row number to the correlation
-          # this can later be accessed via as.character(index)
-          rows_candidates <- rows_candidates[order(similarity_i, decreasing = TRUE)]
+        } else {# at least one candidate row -> proceed with "normal" LSimpute_gene
 
           ## for every missing value j_ind in row i calculate the imputation value separately
           for(j_ind in which(M_i)) {
-            ## find the k suitable rows for imputation and save their index in suitable
-            suitable <- find_suitable_rows(k, rows_candidates, M, M_i, j_ind)
+            ## find the k suitable rows for imputation and save their index from rows_candidates
+            suitable_logic <- !M[rows_candidates$row_index, j_ind] # No NA in j_ind -> suitable
+            suitable_index <- which(suitable_logic)
+            suitable_index <- suitable_index[seq_len(min(k, length(suitable_index)))]
 
-            if (length(suitable) == 0) { # no suitable row found -> impute mean
+            if (length(suitable_index) == 0) { # no suitable row found -> impute mean
               ## this condition may crashes the jar-file from Bo et al. (2004)
               warning("No suitable row for the imputation of row ", i,
                       " and column ", j_ind, " found! Value is imputed with observed row mean.")
@@ -103,15 +101,15 @@ impute_LS_gene <- function(ds, k = 10, eps = 1e-6, min_common_obs = 5,
             } else { # everything fine -> proceed with "normal" LSimpute_gene
 
               if(return_r_max) { # save highest correlation
-                r_max_matrix[i, j_ind] <- similarity_i[as.character(suitable[1])]
+                r_max_matrix[i, j_ind] <- rows_candidates$similarity[suitable_index[1]]
               }
 
-              ## calculate imputation values (with regression) for every suitable row separate and save in y
-              y <- calc_y_LS_gene(ds, i, j_ind, suitable, M , M_i)
+              ## calculate imputation values (with regression) and save them in y
+              y <- calc_y_LS_gene(ds, i, j_ind, rows_candidates, suitable_index, M , M_i)
 
 
               ## calculate weights  ---------------------------------------------
-              similarities_sq <- similarity_i[as.character(suitable)]^2
+              similarities_sq <- rows_candidates$similarity[suitable_index]^2
               w <- (similarities_sq /
                       ( 1- similarities_sq + eps))^2
               w <- w / sum(w)
@@ -136,12 +134,13 @@ impute_LS_gene <- function(ds, k = 10, eps = 1e-6, min_common_obs = 5,
 
 ## helpers for LSimpute_gene --------------------------------------------------
 
-calc_y_LS_gene <- function(ds, i, j_ind, suitable, M = is.na(ds), M_i = M[i, ]) {
-  y <- numeric(length(suitable))
-  for(j in 1:length(suitable)) {
-    common_observed <- !(M_i | M[suitable[j], ])  # this will be at least min_common_obs (>= 3) TRUEs, (requirement for suitable) -> regression possible
-    lm_coef <- calc_lm_coefs_simple_reg(ds[i, common_observed], ds[suitable[j], common_observed])
-    y[j] <- lm_coef[1] + lm_coef[2] * ds[suitable[j], j_ind]
+calc_y_LS_gene <- function(ds, i, j_ind, rows_candidates, suitable_index, M = is.na(ds), M_i = M[i, ]) {
+  y <- numeric(length(suitable_index))
+  row_indices <- rows_candidates$row_index[suitable_index]
+  for(j in seq_along(row_indices)) {
+    common_observed <- !(M_i | M[row_indices[j], ])  # this will be at least min_common_obs (>= 3) TRUEs, (requirement for suitable) -> regression possible
+    lm_coef <- calc_lm_coefs_simple_reg(ds[i, common_observed], ds[row_indices[j], common_observed])
+    y[j] <- lm_coef[1] + lm_coef[2] * ds[row_indices[j], j_ind]
   }
   y
 }
@@ -179,22 +178,20 @@ calc_common_obs <- function(M, M_i) {
   nr_vars - colSums(t(M) | M_i)
 }
 
+find_rows_candidates <- function(ds, i, M = is.na(ds), M_i = M[i, ], min_common_obs) {
+  rows_candidates_index <- which(calc_common_obs(M, M_i) >= min_common_obs & seq_len(nrow(ds)) != i)
 
-find_suitable_rows <- function(k, rows_candidates, M, M_i, j_ind) {
-  kl <- min(k, length(rows_candidates))  # loop variable (number of checked neighbors)
-  ## start with k + 1 neighbors, because row i will be one of the nearest neighbors
-  nr_suitable <- 0
-  while (nr_suitable < k) {
-    potential_genes <- rows_candidates[1:kl]
-    NA_in_j_ind <- M[potential_genes, j_ind]  # missing value in column j_ind (which should be imputed)?
-    suitable <- potential_genes[!NA_in_j_ind] # these rows are not suitable (missing values in column j_ind)
-    nr_suitable <- length(suitable)
-    if (kl >= length(rows_candidates)) {  # to prevent infinite loop, if there are not enough suitable rows
-      # warning(paste("Not enough suitable rows for", i, ",", j_ind))
-      break() # exit loop with the found suitable rows (there are no more additional rows to check!)
-    } else {
-      kl <- kl + 1
-    }
+  if (length(rows_candidates_index) == 0) { # no rows_candidates found
+    return(data.frame(row_index = integer(0)))
   }
-  return(suitable)
+
+  ## calculate correlation
+  similarity_i <- calc_similarity(ds[rows_candidates_index, ], ds[i, ])
+  # order rows by similarity
+  orderd_index <- order(similarity_i, decreasing = TRUE)
+  data.frame(row_index = rows_candidates_index[orderd_index],
+             similarity = similarity_i[orderd_index])
+
 }
+
+
