2a1b30e2d173264ef325ed613eb925af979e3ba2
torockel <rockel.to@gmail.com>
2022-02-08 13:51:22 +0100

    Use mvtnorm::dmvnorm() instead of EMCluster::dmvn()

--- R/impute_GMC.R
+++ R/impute_GMC.R
@@ -5,8 +5,7 @@ weighted_av_gmc <- function(row_values, gmc_parameters, k,
   numerator <- 0
   for (i in seq_len(k)) {
     weighted_density_i <- gmc_parameters$lambda[i] *
-      EMCluster::dmvn(row_values[[i]], gmc_parameters$mu[i, ], gmc_parameters$LTSigma[i, ])
-    weighted_density_i <- ifelse(is.finite(weighted_density_i), weighted_density_i, 0) # https://github.com/snoweye/EMCluster/issues/10
+      mvtnorm::dmvnorm(row_values[[i]], gmc_parameters$mu[i, ], gmc_parameters$sigma[[i]])
     denominator <- denominator + weighted_density_i
     numerator <- numerator + row_values[[i]] * weighted_density_i
   }
