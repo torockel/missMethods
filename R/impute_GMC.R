a1889db26538cc83eec5759a363db1574c529282
torockel <rockel.to@gmail.com>
2022-02-08 13:48:15 +0100

    Issue with EMCluster::dmvn() is Inf

--- R/impute_GMC.R
+++ R/impute_GMC.R
@@ -6,6 +6,7 @@ weighted_av_gmc <- function(row_values, gmc_parameters, k,
   for (i in seq_len(k)) {
     weighted_density_i <- gmc_parameters$lambda[i] *
       EMCluster::dmvn(row_values[[i]], gmc_parameters$mu[i, ], gmc_parameters$LTSigma[i, ])
+    weighted_density_i <- ifelse(is.finite(weighted_density_i), weighted_density_i, 0) # https://github.com/snoweye/EMCluster/issues/10
     denominator <- denominator + weighted_density_i
     numerator <- numerator + row_values[[i]] * weighted_density_i
   }
